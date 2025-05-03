bd9721b448b65b080b8f6c43a922a39dd29c4d6b
nabiladityasaputra <nabiladityasaputra47@gmail.com>
2025-05-03 19:35:06 +0700

    by nabil

--- rental.php
+++ rental.php
@@ -1,103 +1,88 @@
 <?php
-include (".includes/header.php");
-$title = "Dashboard";
-// Menyertakan file untuk menampilkan notifikasi (jika ada)
-include ".includes/toast_notification.php";
+include('.includes/header.php');
+$title = "Data Rental";
+include('.includes/toast_notification.php');
 ?>
 
 <div class="container-xxl flex-grow-1 container-p-y">
-    <!-- Card untuk menampilkan tabel postingan -->
-    <div class="card">
-        <!-- Tabel dengan baris yang dapat di-hover -->
-        <div class="card">
-            <!-- Header Tabel -->
-            <div class="card-header d-flex justify-content-between align-items-center">
-                <h4>DATA PENYEWAAN</h4>
-            </div>
-
-            <div class="card-body">
-                <!-- Tabel responsif -->
-                <div class="table-responsive text-nowrap">
-                    <table id="datatable" class="table table-hover">
-                        <thead>
-                            <tr class="text-center">
-                                <th width="50px">#</th>
-                                <th>Tipe Kendaraan</th>
-                                <th>Ketersediaan</th>
-                                <th>Tanggal kembali</th>
-                                <th width="150px">Pilihan</th>
-                            </tr>
-                        </thead>
-                        <tbody class="table-border-bottom-0">
-                            <!-- Menampilkan data dari tabel database -->
-                            <?php
-                            $index = 1; // Variabel untuk nomor urut
-                            // Query untuk mengambil data dari tabel posts, users, dan category
-                            $query = "SELECT rental.*, kendaraan.tipe as tipe_kendaraan, pelanggan.nomor_lisensi 
-                                      FROM rental 
-                                      INNER JOIN kendaraan ON rental.kendaraan_id = kendaraan.kendaraan_id 
-                                      LEFT JOIN pelanggan ON rental.pelanggan_id = pelanggan.pelanggan_id 
-                                      WHERE pelanggan.pelanggan_id = $userId";
-
-                            // Eksekusi query
-                            $exec = mysqli_query($conn, $query);
-
-                            // Perulangan untuk menampilkan setiap baris hasil query
-                            while ($post = mysqli_fetch_assoc($exec)) :
-                            ?>
-                                <tr>
-                                    <td><?= $index++; ?></td>
-                                    <td><?= $post['tipe']; ?></td>
-                                    <td><?= $post['nomor_lisensi']; ?></td>
-                                    <td><?= $post['total']; ?></td>
-                                    <td>
-                                        <div class="dropdown">
-                                            <!-- Tombol dropdown untuk Pilihan -->
-                                            <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
-                                                <i class="bx bx-dots-vertical-rounded"></i>
-                                            </button>
-                                            <div class="dropdown-menu">
-                                                <!-- Pilihan Edit -->
-                                                <a href="edit_post.php?post_id=<?= $post['id_post']; ?>" class="dropdown-item">
-                                                    <i class="bx bx-edit-alt me-2"></i> Edit
-                                                </a>
-                                                <!-- Pilihan Delete -->
-                                                <a href="#" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#deletePost_<?= $post['id_post']; ?>">
-                                                    <i class="bx bx-trash me-2"></i> Delete
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
-
-                                <!-- Modal untuk Hapus Konten Blog -->
-                     <div class="modal fade" id="deletePost_<?= $post['id_post']; ?>" tabindex="-1" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title">Hapus data?</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <form action="proses_post.php" method="POST">
-                                                    <p>Tindakan ini tidak bisa dibatalkan.</p>
-                                                    <input type="hidden" name="postID" value="<?= $post['id_post']; ?>">
-                                                    <div class="modal-footer">
-                                                        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Batal</button>
-                                                        <button type="submit" name="delete" class="btn btn-primary">Hapus</button>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            <?php endwhile; ?>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
+  <div class="card">
+    <div class="card-header d-flex justify-content-between align-items-center">
+      <h4>Data Rental Kendaraan</h4>
     </div>
+    <div class="card-body">
+      <div class="table-responsive text-nowrap">
+        <table id="datatable" class="table table-hover">
+          <thead class="text-center">
+            <tr>
+              <th>#</th>
+              <th>Nama Pelanggan</th>
+              <th>Kendaraan</th>
+              <th>Tanggal Rental</th>
+              <th>Tanggal Kembali</th>
+              <th>Total (Rp)</th>
+              <th>Aksi</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            $query = "SELECT rental.*, pelanggan.nama AS nama, kendaraan.tipe, kendaraan.model
+                      FROM rental
+                      LEFT JOIN pelanggan ON rental.pelanggan_id = pelanggan.pelanggan_id
+                      LEFT JOIN kendaraan ON rental.kendaraan_id = kendaraan.kendaraan_id";
+            $result = mysqli_query($conn, $query);
+            $no = 1;
+            while ($rental = mysqli_fetch_assoc($result)) :
+            ?>
+              <tr class="text-center">
+                <td><?= $no++; ?></td>
+                <td><?= htmlspecialchars($rental['nama']); ?></td>
+                <td><?= $rental['tipe'] . ' - ' . $rental['model']; ?></td>
+                <td><?= $rental['tgl_rental']; ?></td>
+                <td><?= $rental['tgl_kembali']; ?></td>
+                <td>Rp <?= number_format($rental['total'], 0, ',', '.'); ?></td>
+                <td>
+                  <button type="button" class="btn btn-sm btn-primary dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                    <i class="bx bx-dots-vertical-rounded"></i>
+                  </button>
+                  <div class="dropdown-menu">
+                    <a class="dropdown-item" href="edit_rental.php?rental_id=<?= $rental['rental_id']; ?>">
+                      <i class="bx bx-edit-alt me-1"></i> Edit
+                    </a>
+                    <a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#deleteRental_<?= $rental['rental_id']; ?>">
+                      <i class="bx bx-trash me-1"></i> Hapus
+                    </a>
+                  </div>
+
+                  <!-- Modal Hapus -->
+                  <div class="modal fade" id="deleteRental_<?= $rental['rental_id']; ?>" tabindex="-1" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <form action="proses_rental.php" method="POST">
+                          <div class="modal-header">
+                            <h5 class="modal-title">Hapus Data Rental?</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                          </div>
+                          <div class="modal-body">
+                            <p>Apakah kamu yakin ingin menghapus data ini? Tindakan ini tidak dapat dibatalkan.</p>
+                            <input type="hidden" name="rental_id" value="<?= $rental['rental_id']; ?>">
+                          </div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Batal</button>
+                            <button type="submit" name="delete_rental" class="btn btn-danger">Hapus</button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+
+                </td>
+              </tr>
+            <?php endwhile; ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </div>
 
-<?php include (".includes/footer.php"); ?>
+<?php include('.includes/footer.php'); ?>
